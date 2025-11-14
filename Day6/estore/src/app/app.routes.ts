3551cc237dfcc0a354cc06297950c87d9f587039
KajalGhule <kajalghule111@gmail.com>
2025-11-14 17:19:32 +0530

    estore application

--- Day6/estore/src/app/app.routes.ts
+++ Day6/estore/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/products', pathMatch: 'full' },
+    { path: 'products', loadChildren: () => import('./product-catalog/product-catalog.module').then(m => m.ProductCatalogModule) },
+];
