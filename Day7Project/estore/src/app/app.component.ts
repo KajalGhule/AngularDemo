c5b8518d7e42ea7004152ffdf8663539f3b40861
KajalGhule <kajalghule111@gmail.com>
2025-11-15 16:14:02 +0530

    code cleaning

--- Day7Project/estore/src/app/app.component.ts
+++ Day7Project/estore/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MembershipModule } from './membership/membership.module';
-import { ProductCatalogModule } from './product-catalog/product-catalog.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MembershipModule, ProductCatalogModule],
+  imports: [RouterOutlet, CommonModule, MembershipModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Day7Project/estore/src/app/app.routes.ts
+++ Day7Project/estore/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/products', pathMatch: 'full' },
-    { path: 'products', loadChildren: () => import('./product-catalog/product-catalog.module').then(m => m.ProductCatalogModule) },
+    // { path: 'products', loadChildren: () => import('./product-catalog/product-catalog.module').then(m => m.ProductCatalogModule) },
 ];
